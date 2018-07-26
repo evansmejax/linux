bd5fee57bf614fd6195e4d006935dda6aebcbe7e
Kiplagat Evans Kiprotich <35095785+evansMeja@users.noreply.github.com>
2018-07-26 17:49:07 +0300

    Update kernel.cpp

--- kernel.cpp
+++ kernel.cpp
@@ -1,9 +1,24 @@
-#include<stdlib.h>
-#include<stdio.h>
 
-void kernelMain(void* multiboot_stucture,unsigned int magicnumber){
-  printf("Hello world ---http://www.programmingempire.tk");
-  while(1){
-  
-  } 
+//main kernel function
+void printf(char* str)
+{
+    static unsigned short* VideoMemory = ( unsigned short* )0xb8000;
+    for(int i=0;str[i] !='\0'; ++i){
+      VideoMemory[i]= (VideoMemory[i] & 0xFF00) | str[i];
+      
+ }
+ 
 }
+
+//params accepting values on top of stack
+//extern "C" makes function available for linker
+extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+{
+  printf("crazy programmer evans meja\n");
+  printf("We are now in kernel main\n");
+  printf("crazy programmer evans meja\n");
+  printf("https://www.programmingempire.tk/\n");
+  printf("crazy programmer evans meja\n");
+  while(1);
+}
+
